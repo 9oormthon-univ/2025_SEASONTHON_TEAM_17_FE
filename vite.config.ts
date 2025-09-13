d277ee6afe228fa33ebd0dd446c7a9418043af82
Dubabbi <thdms3160@naver.com>
2025-09-14 03:45:11 +0900

    chore: 필요없는 패키지 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,13 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
-import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   build: {
     sourcemap: false,
-    minify: 'esbuild',
-    cssMinify: true,
+    minify: false,
+    cssMinify: false,
     assetsInlineLimit: 0,
     chunkSizeWarningLimit: 1500,
     rollupOptions: {
@@ -38,10 +37,6 @@ export default defineConfig({
     createSvgIconsPlugin({
       iconDirs: [path.resolve(process.cwd(), 'src/shared/assets/icons')],
       symbolId: 'icon-[name]',
-    }),
-    svgr({
-      include: ['src/**/*.svg'],
-      exclude: ['**/shared/assets/icons/**', 'public/**'],
     }),
     VitePWA({
       strategies: 'generateSW',
