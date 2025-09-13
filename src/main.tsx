d5afd881e53cec1c3a124e988ed903ec0591a78e
Dubabbi <thdms3160@naver.com>
2025-09-14 02:29:06 +0900

    feat: main.tsx 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './shared/styles/index.css';
 import App from './App';
+import 'virtual:svg-icons-register';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Root element "#root" not found');
